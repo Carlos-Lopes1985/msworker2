3e92e43c224c72ef7bf5e0695d140cc6bdd1f5c9
Carlos-Lopes1985 <carlosslopes1985@hotmail.com>
2021-02-02 20:17:08 -0300

    primeiro commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES('bob', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES('Ana', 100.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES('jorge', 300.0);
